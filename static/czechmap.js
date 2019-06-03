0f3394c7f557276096279321c0a1bd12db3e5c93
Zuzana Pelechova <zpelechova@gmail.com>
2019-06-03 21:40:05 +0200

    navrat mapicky k jakztakz funkcni variante

--- static/czechmap.js
+++ static/czechmap.js
@@ -2,16 +2,35 @@
 //Contact: matousek.vr@gmail.com
 //License: GPL
 
- $(function(){
 
-    $('.czechMap-area').hover(function(){
-
-        $('#czechMap-overlap').attr('src', $(this).attr('data-img') );
-
-    },function(){
-
-        $('#czechMap-overlap').attr('src', '/static/none.png');
-    });
-
- });
-
+  $(function(){
+     var czechMapOverlap = $('#czechMap-overlap'),
+         czechMap = $('#czechMap'); 
+     
+     // map hover 
+     $('.czechMap-area').hover(function(){
+         
+         czechMapOverlap.attr('src', $(this).attr('data-img') );
+         czechMapOverlap.hide(0).stop(false, true);
+         czechMapOverlap.fadeIn(500);
+         
+     },function(){
+         
+         czechMapOverlap.attr('src', '/static/none.png');
+         czechMapOverlap.show(0);
+     });
+     
+     // links hover
+     $('#czechMapLinks a').hover(function(){
+     
+         czechMapOverlap.attr('src', czechMap.find('[href="' + $(this).attr('href') + '"]').attr('data-img') );
+         czechMapOverlap.hide(0).stop(false, true);
+         czechMapOverlap.fadeIn(300);
+         
+     },function(){
+         
+         czechMapOverlap.attr('src', '/static/none.png');
+         czechMapOverlap.show(0);
+         
+     });
+  });
